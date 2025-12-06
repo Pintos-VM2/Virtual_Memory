307f01941c5cee788a84f2829411a66a1f40ac12
uyeong <uyeong1575@gmail.com>
2025-12-06 23:31:54 +0900

    [박우영] anon swap in/out 구현

--- pintos/vm/anon.c
+++ pintos/vm/anon.c
@@ -3,6 +3,9 @@
 #include "vm/vm.h"
 #include "devices/disk.h"
 
+#define SECTOR_UNIT 8 //(PGSIZE / DISK_SECTOR_SIZE)
+struct bitmap *swap_bm;
+
 /* DO NOT MODIFY BELOW LINE */
 static struct disk *swap_disk;
 static bool anon_swap_in (struct page *page, void *kva);
@@ -20,8 +23,11 @@ static const struct page_operations anon_ops = {
 /* Initialize the data for anonymous pages */
 void
 vm_anon_init (void) {
-	/* TODO: Set up the swap_disk. */
-	swap_disk = NULL;
+	/* Set up the swap_disk. */
+	swap_disk = disk_get(1,1);
+	/* swap slot 관리 bitmap 세팅 */
+	size_t swap_slot_cnt = disk_size(swap_disk) * DISK_SECTOR_SIZE / PGSIZE;
+	swap_bm = bitmap_create(swap_slot_cnt);
 }
 
 /* Initialize the file mapping */
@@ -34,26 +40,69 @@ anon_initializer (struct page *page, enum vm_type type, void *kva) {
 	struct anon_page *anon_page = &page->anon;
 	anon_page->type = type;
 
-	/* IS_STACK 이면 stack setting */
-	if (type & IS_STACK)
+	if (type & IS_STACK)	// IS_STACK 이면 stack setting
 		memset(kva, 0, PGSIZE);
-
-	/* todo : 그냥 ANON이면 추가 */
+	
+	anon_page->swap_slot_idx = BITMAP_ERROR;
 
 	return true;
 }
 
 /* Swap in the page by read contents from the swap disk. */
+// kva일단 할당 되었다고 가정
 static bool
 anon_swap_in (struct page *page, void *kva) {
 	struct anon_page *anon_page = &page->anon;
-	//return true;
+	int cnt;
+
+	//idx 가져오기
+	size_t idx = anon_page->swap_slot_idx;
+	if(idx == BITMAP_ERROR) return false;
+	
+	//idx로부터 kva로 disk_read
+	int i = SECTOR_UNIT;
+	while(i--){
+		cnt = SECTOR_UNIT-i;
+		disk_read(swap_disk, idx+cnt, kva+(cnt*DISK_SECTOR_SIZE));
+	}
+
+	//bitmap 0으로 만들고 anon_page update
+	bitmap_set_multiple(swap_bm, idx, SECTOR_UNIT, 0);
+	anon_page->swap_slot_idx = BITMAP_ERROR;
+
+	//page에 물리 프레임 연결, frame table에 추가, pml4 매핑 비트 on
+	page->frame->kva = kva;
+	pml4_set_page(thread_current()->pml4, page->va, kva, page->writable);
+	list_push_back(&frame_list, &page->frame->frame_elem);
+
+	return true;
 }
 
 /* Swap out the page by writing contents to the swap disk. */
 static bool
 anon_swap_out (struct page *page) {
 	struct anon_page *anon_page = &page->anon;
+	int cnt;
+
+	//swap_disk에서 빈 공간 찾기
+	size_t idx = bitmap_scan_and_flip(swap_bm, 0, SECTOR_UNIT, 0);
+	if(idx == BITMAP_ERROR) return false;
+
+	// 해당 공간에 disk_write, idx 기록
+	int i = SECTOR_UNIT;
+	void *kva = page->frame->kva;
+	while(i--){
+		cnt = SECTOR_UNIT-i;
+		disk_write(swap_disk, idx+cnt, kva+(cnt*DISK_SECTOR_SIZE));
+	}
+	anon_page->swap_slot_idx = idx;
+
+	//물리 프레임 회수, pml4 매핑 비트 off
+	pml4_clear_page(thread_current()->pml4, page->va);
+	list_remove(&page->frame->frame_elem);
+	palloc_free_page(page->frame->kva);
+
+	return true;
 }
 
 /* Destroy the anonymous page. PAGE will be freed by the caller. */
@@ -63,5 +112,9 @@ anon_destroy (struct page *page) {
 	
 	/* anon_page 구조체 만들어지면 free 추가*/
 
-	// palloc_free_page(page->frame->kva);
+	pml4_clear_page(thread_current()->pml4, page->va);
+
+	list_remove(&page->frame->frame_elem);
+
+	palloc_free_page(page->frame->kva);
 }
